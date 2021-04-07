73334e2beae29fd03f0cb2ecfcd29bf2c5e34a9a
Benoit Patry <benoit.patry.1@ulaval.ca>
2021-04-07 13:37:33 -0400

    test page blanche

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-function Test() {
+export default function Test() {
     const title = 'NETdra.com'
 
     return (
         <div className='lmj-banner'>
             <h1>{title}</h1>
         </div>
-    )
+    );
 }
-
-export default Test
