b3c1c398bfcd9162eab4cd55f24868b521be457a
Benoit Patry <benoit.patry.1@ulaval.ca>
2021-04-07 09:13:07 -0400

    Titre

---
+++ src/components/test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Test() {
+    const title = 'NETdra.com'
+
+    return (
+        <div className='lmj-banner'>
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
+export default Test
