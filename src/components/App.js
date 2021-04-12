5e45913284f1fcceacccf8ff714ee49f94ca12e4
deawoo18 <deawoo18@hotmail.com>
2021-04-12 08:25:47 -0400

    Test google Analytique

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Header from './headers.component';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
